b89c08fc541e051be1f61b0f8fd9817fbcbf987c
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2021-03-06 20:00:46 +0900

    画像圧縮、imgタグのretinaディスプレイ対応

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,76 @@
 // プラグインの読み込み
 const gulp = require('gulp');
+
+// sassプラグインの読み込み
 const sass = require('gulp-sass');
 sass.compiler = require('sass'); // dart-sassを使用
 const sassGlob = require('gulp-sass-glob');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+
+// 画像圧縮プラグインの読み込み
+var imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+const changed = require('gulp-changed');
+
+// ディレクトリ定義
+var paths = {
+  sassSrc: 'sass/style.scss', // sassのソースディレクトリ
+  sassDst: 'public/css', // sassのコンパイルファイルの保存先ディレクトリ
+  imageSrc: 'images', // 画像のソースディレクトリ
+  imageDst: 'public/images', // 圧縮画像の保存先ディレクトリ
+};
 
 // Sassコンパイルタスクの定義
 const compile = () => {
   return gulp
-    .src('sass/style.scss')
+    .src(paths.sassSrc, { sourcemaps: true })
     .pipe(
       plumber({
         errorHandler: notify.onError('Error: <%= error.message %>'),
       })
     )
-    .pipe(sourcemaps.init())
     .pipe(sassGlob()) // Sassの@importにおけるglobを有効にする
     .pipe(sass())
     .pipe(
       autoprefixer(['last 3 versions', 'ie >= 8', 'Android >= 4', 'iOS >= 8'])
     )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest(paths.sassDst, { sourcemaps: '.' }));
 };
 
 // ファイル変更監視タスクの定義
-const watch = () => {
+const watchSass = () => {
   return gulp.watch('sass/**', compile);
 };
 
+// 画像の圧縮タスク
+const imageminTask = () => {
+  var srcGlob = paths.imageSrc + '/**/*.+(jpg|jpeg|png|gif)';
+  var dstGlob = paths.imageDst;
+  return gulp
+    .src(srcGlob)
+    .pipe(changed(dstGlob))
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [0.7, 0.85], // 画質
+          speed: 1, // スピード
+        }),
+        mozjpeg({
+          quality: 85, // 画質
+          progressive: true,
+        }),
+      ])
+    )
+    .pipe(gulp.dest(dstGlob));
+};
+
+// ファイル変更監視タスクの定義
+const watchImagemin = () => {
+  return gulp.watch(paths.imageSrc + '/**/*', gulp.parallel(imageminTask));
+};
+
 // ファイル変更監視タスクのエクスポート
-exports.watch = watch;
+exports.watch = gulp.parallel(watchSass, watchImagemin);
