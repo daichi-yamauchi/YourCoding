6ecb1791e6c358bd4b1ad0c4afe4eea7a7cb6eb2
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2021-03-07 10:03:14 +0900

    swiperのオートプレイ対応、ボタン追加

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -14,15 +14,22 @@ $(function () {
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   centeredSlides: true,
   loop: true,
+  autoplay: {
+    delay: 5000,
+  },
   breakpoints: {
     640: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 40,
     },
     1024: {
