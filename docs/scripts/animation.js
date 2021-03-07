34a2757a35dff41108b84c8547df94ce0a60930f
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2021-03-07 17:10:16 +0900

    アニメーションの追加

---
+++ docs/scripts/animation.js
@@ -0,0 +1,27 @@
+/**********
+ * AOS.js *
+ **********/
+
+$('section').attr({
+  'data-aos': 'fade-up',
+});
+
+$('.p-feature-outer:nth-child(even)').attr({
+  'data-aos': 'fade-left',
+});
+$('.p-feature-outer:nth-child(odd)').attr({
+  'data-aos': 'fade-right',
+});
+
+$('.p-article-item').attr({
+  'data-aos': 'flip-left',
+});
+
+if (window.innerWidth > 1024) {
+  for (let n = 0; n < 3; n++)
+    $(`.p-article-item:nth-child(${n + 1})`).attr({
+      'data-aos-delay': 200 * n,
+    });
+}
+
+AOS.init();
